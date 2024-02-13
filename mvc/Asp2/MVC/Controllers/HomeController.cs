0021622372d56fb118b20d8957abd3ca0375d4b8
Václav Hataš <vaclav.hatas@online.txn.cz>
2024-02-13 10:38:49 +0100

    mvc hello world

--- mvc/Asp2/MVC/Controllers/HomeController.cs
+++ mvc/Asp2/MVC/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MVC.Controllers
         {
             return View();
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
